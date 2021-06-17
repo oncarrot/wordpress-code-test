43047994fbdb0751e9426ee6542489c2cd8372fb
Jonathan Spooner <jspooner@gmail.com>
2021-06-17 12:14:34 -0700

    WIP

--- entrypoint-initdb/test.sql
+++ entrypoint-initdb/test.sql
@@ -1,1 +1,19 @@
+
+select "HELLO";
+select "HELLO";
+select "HELLO";
+select "HELLO";
+select "Running test.sql";
+select "Running test.sql";
+select "Running test.sql";
+select "Running test.sql";
+select "Running test.sql";
+select "Running test.sql";
+select "Running test.sql";
+select "Running test.sql";
+select "HELLO";
+select "HELLO";
+select "HELLO";
+select "HELLO";
+select "HELLO";
 SELECT * FROM wp_users;
